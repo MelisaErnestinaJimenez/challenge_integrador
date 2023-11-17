c49341b4ad75c59af1b50e28f9d6d13f80ebe836
Melisa Ernestina Jimenez <melisaernestinajimenez@gmail.com>
2023-11-17 00:03:40 -0300

    arreglos en el menu hamburguesa

--- javascript/menuhamb.js
+++ javascript/menuhamb.js
@@ -2,12 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.getElementById('menu-toggle');
     const navbarMenu = document.querySelector('.navbar__menu');
 
-    menuToggle.addEventListener('click', function () {
+    // Función para manejar clic y eventos táctiles
+    const toggleMenu = function () {
         navbarMenu.classList.toggle('active');
-    });
+    };
 
-    //  tactil
-    menuToggle.addEventListener('touchstart', function () {
-        navbarMenu.classList.toggle('active');
-    });
+    // Agrega el evento de clic
+    menuToggle.addEventListener('click', toggleMenu);
+
+    // Agrega el evento táctil
+    menuToggle.addEventListener('touchstart', toggleMenu);
 });
