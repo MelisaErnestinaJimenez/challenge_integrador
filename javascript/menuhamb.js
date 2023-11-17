8081c69754afb877e0b1ccd893076ae96fd014ea
Melisa Ernestina Jimenez <melisaernestinajimenez@gmail.com>
2023-11-17 00:06:14 -0300

    Arreglos en header

--- javascript/menuhamb.js
+++ javascript/menuhamb.js
@@ -2,14 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.getElementById('menu-toggle');
     const navbarMenu = document.querySelector('.navbar__menu');
 
-    // Función para manejar clic y eventos táctiles
     const toggleMenu = function () {
         navbarMenu.classList.toggle('active');
     };
 
-    // Agrega el evento de clic
-    menuToggle.addEventListener('click', toggleMenu);
-
-    // Agrega el evento táctil
     menuToggle.addEventListener('touchstart', toggleMenu);
 });
